24f0f3c96ae2e26b7bf857cf941596432b2ad02b
Martin Törnqvist <m.tornq@gmail.com>
2022-09-07 16:39:25 +0200

    Set version to v22.0.0-beta1

--- src/version.cpp
+++ src/version.cpp
@@ -13,7 +13,7 @@
 
 namespace version_info
 {
-const std::string g_version_str = "v21.0.1";
+const std::string g_version_str = "v22.0.0-beta1";
 
 const std::string g_copyright_str =
         "(c) 2011-2022 Martin Tornqvist";
