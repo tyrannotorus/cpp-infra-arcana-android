50650d2487c54f91ca5af4be238775373430f679
Martin Törnqvist <m.tornq@gmail.com>
2024-06-22 17:22:16 +0200

    Update copyright year

--- src/inventory_handling.cpp
+++ src/inventory_handling.cpp
@@ -1,5 +1,5 @@
 // =============================================================================
-// Copyright 2011-2023 Martin Törnqvist <m.tornq@gmail.com>
+// Copyright 2011-2024 Martin Törnqvist <m.tornq@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // =============================================================================
