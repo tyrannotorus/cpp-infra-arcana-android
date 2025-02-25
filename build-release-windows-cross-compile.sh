7c0c999182321cce8169b3119cf54077c58496da
Martin Törnqvist <m.tornq@gmail.com>
2025-02-25 20:15:16 +0100

    Disable static SDL in windows release build script

--- build-release-windows-cross-compile.sh
+++ build-release-windows-cross-compile.sh
@@ -7,7 +7,7 @@ root_dir=${PWD}
 rm -rf build
 mkdir build
 cd build
-cmake -DIA_BUILD_STATIC_SDL=ON -DWIN32=TRUE -DMSVC=FALSE -DARCH=64bit -DCMAKE_TOOLCHAIN_FILE=../Toolchain-cross-mingw32.txt ..
+cmake -DIA_BUILD_STATIC_SDL=OFF -DWIN32=TRUE -DMSVC=FALSE -DARCH=64bit -DCMAKE_TOOLCHAIN_FILE=../Toolchain-cross-mingw32.txt ..
 VERBOSE=1 cmake --build . --target install -- -j$(nproc)
 
 cd ${root_dir}
